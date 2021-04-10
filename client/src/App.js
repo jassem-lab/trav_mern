ff6101f0a423baf407cde0bfadbb8616cc40c26f
jassem-lab <jassemgaaloul123@gmail.com>
2021-04-10 04:17:52 +0100

    💄 Add an SVG as a Marker and customize it

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,24 @@ const App = () => {
             offsetLeft={-20}
             offsetTop={-10}
           >
-            <div>{entry.title}</div>
+            <svg
+              viewBox='0 0 24 24'
+              width='48'
+              style={{
+                width: `calc(1vmin * ${viewport.zoom})`,
+                height: `calc(1vmin * ${viewport.zoom})`,
+              }}
+              height='48'
+              stroke='currentColor'
+              stroke-width='1.5'
+              fill='none'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              className='Marker'
+            >
+              <path d='M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z'></path>
+              <circle cx='12' cy='10' r='3'></circle>
+            </svg>{' '}
           </Marker>
         ))}
       </ReactMapGL>
